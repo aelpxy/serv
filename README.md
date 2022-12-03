6b2ebc75643343ebe907fd59e944717857dadfd9
Aelpxy <84912564+Aelpxy@users.noreply.github.com>
2022-12-03 17:17:50 +0000

    feat: implemented the core

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
 # broccoli
+
+---
+
+# Building
+
+```
+go build
+```
+
+# Usage
+
+```
+  broccoli [command]
+
+Available Commands:
+  completion  Generate the autocompletion script for the specified shell
+  help        Help about any command
+  serve       Serve the application
+
+Flags:
+  -h, --help   help for broccoli
+
+Use "broccoli [command] --help" for more information about a command.
+```
+
+# License
+## [MIT](./LICENSE)
--- main.go
+++ main.go
@@ -1,9 +1,80 @@
 package main
 
 import (
+	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/spf13/cobra"
+)
+
+var (
+	application_port string
 )
 
 func main() {
-	log.Println("broccoli")
+	cmd := &cobra.Command{
+		Use:   "serve [port]",
+		Short: "Serve the application",
+		Long:  "Serve the application",
+		Run: func(cmd *cobra.Command, args []string) {
+			if application_port == "" {
+				log.Println("Port is missing")
+			}
+
+			log.Println("Starting...")
+
+			g := http.NewServeMux()
+			g.Handle("/", http.FileServer(http.Dir("files")))
+			g.HandleFunc("/upload", uploadHandler)
+
+			log.Printf("Listening on http://0.0.0.0:%v \n", application_port)
+
+			if err := http.ListenAndServe(":"+application_port, g); err != nil {
+				log.Fatal(err)
+			}
+		},
+	}
+
+	cmd.Flags().StringVarP(&application_port, "port", "p", "", "Port to expose database on.")
+
+	rootCmd := &cobra.Command{Use: "broccoli"}
+	rootCmd.AddCommand(cmd)
+	rootCmd.Execute()
+}
+
+func uploadHandler(w http.ResponseWriter, r *http.Request) {
+	file, fileHeader, err := r.FormFile("file")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	defer file.Close()
+
+	err = os.MkdirAll("./files", os.ModePerm)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	dst, err := os.Create(fmt.Sprintf("./files/%d%s", time.Now().UnixNano(), filepath.Ext(fileHeader.Filename)))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	defer dst.Close()
+
+	_, err = io.Copy(dst, file)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	fmt.Fprintf(w, "Success!")
 }
